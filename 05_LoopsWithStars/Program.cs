527c1e5a7b2c9a570b4d291441674bbe2f467fc8
Hüseyin TOPBAŞ <huseyintopba@gmail.com>
2024-10-17 01:18:30 +0300

    5. Derste işlenen konular yapıldı

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 yıldız yazdırma
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+
+            #region Yan yana 10 yıldız yazdırma
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+            #endregion
+
+            #region Alt alta 10 yıldız her satırda 10 tane olacak şekilde yazdırma
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region yıldızlardan dik üçgen şeklinde yazdırma
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region yıldızlardan ters dik üçgen şeklinde yazdırma
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region yıldızlardan elmas şeklinde yazdırma
+
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+            #region yıldızlardan piramit şeklinde yazdırma
+            //int n = 5;
+
+            //for (int i = 1; i <=n; i++)
+            //{
+            //    for (int j = n-i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region yıldızlardan ters piramit şeklinde yazdırma
+            //int n = 5;
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    for (int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
